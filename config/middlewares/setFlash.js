6c49f6dca8a027a946182fb3bc984cd4cec07eba
saumitrarajpoot <saumitrarajpoot@gmail.com>
2014-11-29 20:19:36 +0530

    add flash message issue fix

--- config/middlewares/setFlash.js
+++ config/middlewares/setFlash.js
@@ -1,7 +1,7 @@
 function setFlash(req,res,next) {
   res.locals.flash = {
-    notice: (req.flash('notice') === undefined ) ? res.flash('notice') : req.flash('notice'),
-    error: (req.flash('error')  === undefined ) ? res.flash('error') : req.flash('error')
+    notice: req.flash('notice'),
+    error: req.flash('error')
   }
   next()
 }
